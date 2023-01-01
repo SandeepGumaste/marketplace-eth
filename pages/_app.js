1a97a5d79de2532da1610046a865378a6d5c8b35
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2023-01-02 02:02:43 +0530

    base layout added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { BaseLayout } from "components/index";
 import "styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const Layout = Component.Layout ?? BaseLayout;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,22 @@
 import {
+  BaseLayout,
   Breadcrumbs,
-  Card,
   CourseList,
   EthRates,
-  Footer,
   Hero,
-  Navbar,
   OrderCard,
   WalletBar,
 } from "components";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">
-            <Hero />
-            <Breadcrumbs />
-            <WalletBar />
-            <EthRates />
-            <OrderCard />
-            <CourseList />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
+    <>
+      <Hero />
+      <Breadcrumbs />
+      <WalletBar />
+      <EthRates />
+      <OrderCard />
+      <CourseList />
+    </>
   );
 }
