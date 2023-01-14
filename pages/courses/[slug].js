bc99f6150554f7bc50257a56a22aa85564812e2e
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2023-01-15 00:26:16 +0530

    get single course

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -5,10 +5,12 @@ import {
   KeyPoints,
   Modal,
 } from "components";
+import { getAllCourses } from "content";
 
-export default function Course() {
+export default function Course({ course }) {
   return (
     <>
+      {course.title}
       <div className="py-4">
         <CourseHero />
       </div>
@@ -17,4 +19,27 @@ export default function Course() {
       <Modal />
     </>
   );
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses();
+
+  return {
+    paths: data.map((c) => ({
+      params: {
+        slug: c.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  const { data } = getAllCourses();
+  const course = data.filter((c) => c.slug === params.slug)[0];
+  return {
+    props: {
+      course,
+    },
+  };
 }
